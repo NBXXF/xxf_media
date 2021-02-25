f4938f8ae331b47738b7893fd1d70a6d687acff8
XXF <xuanyouwu@163.com>
2021-02-25 17:16:30 +0800

    add feat: preview

---
+++ preview/src/main/java/com/xxf/media/preview/loader/VideoClickListener.java
@@ -0,0 +1,16 @@
+package com.xxf.media.preview.loader;
+
+
+/**
+ * author  yangc
+ * date 2018/6/28
+
+ * Deprecated:
+ *
+ */
+public   interface VideoClickListener {
+
+     void onPlayerVideo(String url);
+
+
+}
