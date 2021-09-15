648cb8f51309102ae2787118b1adbafc9803347f
xxf <xuanyouwu@163.com>
2021-09-15 18:08:44 +0800

    feat:加载高清图

--- lib_previewer/src/main/java/com/xxf/media/previewer/ui/PreviewFragment.kt
+++ lib_previewer/src/main/java/com/xxf/media/previewer/ui/PreviewFragment.kt
@@ -9,7 +9,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.DrawableImageViewTarget
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.xxf.media.photoview.PhotoViewAttacher
 import com.xxf.media.previewer.Config
@@ -74,6 +78,29 @@ open class PreviewFragment : Fragment() {
                 .thumbnail(
                     Glide.with(this)
                         .load(url.url)
+                        .priority(Priority.IMMEDIATE)
+                        .addListener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                requireActivity().supportStartPostponedEnterTransition()
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                dataSource: DataSource?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                requireActivity().supportStartPostponedEnterTransition()
+                                return false
+                            }
+                        })
                 );
             if (url.placeholderResourceId > 0) {
                 request = request.placeholder(url.placeholderResourceId)
