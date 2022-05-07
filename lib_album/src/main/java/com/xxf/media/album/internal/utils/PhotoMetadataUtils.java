21c183295b367465616b2fd63f7cd063bd3a0fef
xxf <xuanyouwu@163.com>
2022-05-07 20:08:15 +0800

    fix:PhotoMetadataUtils.showRotate方法崩溃

--- lib_album/src/main/java/com/xxf/media/album/internal/utils/PhotoMetadataUtils.java
+++ lib_album/src/main/java/com/xxf/media/album/internal/utils/PhotoMetadataUtils.java
@@ -158,7 +158,7 @@ public final class PhotoMetadataUtils {
         ExifInterface exif;
         try {
             exif = ExifInterfaceCompat.newInstance(getPath(resolver, uri));
-        } catch (IOException e) {
+        } catch (Throwable e) {
             Log.e(TAG, "could not read exif info of the image: " + uri);
             return false;
         }
